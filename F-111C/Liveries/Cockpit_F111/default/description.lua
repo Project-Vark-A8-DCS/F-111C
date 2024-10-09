3b06130659e991886b3e653ce2c37225fa36cd04
James Young <17939249+jamesy0ung@users.noreply.github.com>
2024-10-09 21:55:10 +1100

    Remove old shit

--- F-111C/Liveries/Cockpit_F111/default/description.lua
+++
@@ -1,4 +0,0 @@
-livery = 
-{
-	{"f_111f_mirrors", 0 ,"mirrors",true};--mirrors texture replacement with runtime rendered one
-}
--- F-111C/Options/optionsData.lua
+++
@@ -1,7 +0,0 @@
-cdata =
-{
-    modName                 = _('F111'),
-    CPDICT                  = _('CUSTOMIZED COCKPIT'),
-    default                 = _('Default_liv'),
-    english                 = _('English_liv'),
-}
